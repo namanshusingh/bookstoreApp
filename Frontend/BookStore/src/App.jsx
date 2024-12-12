9ad01d21a962f4eb411db01b9052cf7016f3b677
namanshusingh <namanshu02@gmail.com>
2024-12-12 21:36:30 +0530

    backend done

--- Frontend/BookStore/src/App.jsx
+++ Frontend/BookStore/src/App.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import Home from "./Home/Home";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Courses from "./Courses/Courses";
 import Signup from "./Components/Signup";
+import {Toaster} from "react-hot-toast"
+import { useAuth } from "./context/AuthProvider";
 export default function App() {
+  const [authUser,setAuthUser]=useAuth()
+  console.log(authUser);
   return (
     <>
       <div className="dark:bg-slate-900 dark:text-white">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/course" element={<Courses />} />
+          <Route path="/course" element={authUser?<Courses />:<Navigate to="/signup"></Navigate>} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
+        <Toaster />
       </div>
     </>
   );
--- Frontend/BookStore/src/Components/Freebook.jsx
+++ Frontend/BookStore/src/Components/Freebook.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import list from "../../public/list.json";
+
+import axios from "axios";
 import Cards from "./Cards";
 export default function Freebook() {
-  const filterData = list.filter((data) => data.category === "free");
+  const [book, setBook] = useState([]);
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+        const res = await axios.get("http://localhost:4001/book");
+        console.log(res.data);
+        setBook(res.data.filter((data) => data.category === "free"));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBook();
+  }, []);
+  
   var settings = {
     dots: true,
     infinite: false,
@@ -57,7 +71,7 @@ export default function Freebook() {
       </div>
       <div>
         <Slider {...settings}>
-       {filterData.map((item)=>(
+       {book.map((item)=>(
         <Cards item = {item} key={item.id} />
        ))}
       </Slider>
